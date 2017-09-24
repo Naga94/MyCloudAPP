ebeb9367a9cc5b3f54d3da456063c73aebee9ae4
Naga94 <naanaa.sn@gmail.com>
2017-09-24 20:36:31 +0530

    EnumDemo Uploaded

---
+++ EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnumDemo
+{
+    class EnumDemo
+    {
+        enum IIMCA
+        {GODWIN,ALEX,JEYA,PRASANNA}
+        public static void Main(string[] args)
+        {
+            string name1=Convert.ToString( IIMCA.ALEX);
+            string name2=Convert.ToString(IIMCA.GODWIN);
+            Console.WriteLine(name1);
+            Console.WriteLine(name2);
+            Console.ReadLine();
+        }
+    }
+}
